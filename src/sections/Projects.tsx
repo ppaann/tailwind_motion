93be1155a22296cdf68ede2acecf0d5993d9c555
ppaann <reg.ppan@gmail.com>
2025-01-28 14:10:01 -0800

    project secion ui

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -1,46 +1,117 @@
-import darkSaasLandingPage from "@/assets/images/dark-saas-landing-page.png";
-import lightSaasLandingPage from "@/assets/images/light-saas-landing-page.png";
-import aiStartupLandingPage from "@/assets/images/ai-startup-landing-page.png";
+import darkSaasLandingPage from '@/assets/images/dark-saas-landing-page.png';
+import lightSaasLandingPage from '@/assets/images/light-saas-landing-page.png';
+import aiStartupLandingPage from '@/assets/images/ai-startup-landing-page.png';
+import Image from 'next/image';
+import RightUpArrowIcon from '@/assets/icons/arrow-up-right.svg';
+import grainImage from '@/assets/images/grain.jpg';
+import CheckCircleIcon from '@/assets/icons/check-circle.svg';
 
 const portfolioProjects = [
   {
-    company: "Acme Corp",
-    year: "2022",
-    title: "Dark Saas Landing Page",
+    company: 'Acme Corp',
+    year: '2022',
+    title: 'Dark Saas Landing Page',
     results: [
-      { title: "Enhanced user experience by 40%" },
-      { title: "Improved site speed by 50%" },
-      { title: "Increased mobile traffic by 35%" },
+      { title: 'Enhanced user experience by 40%' },
+      { title: 'Improved site speed by 50%' },
+      { title: 'Increased mobile traffic by 35%' },
     ],
-    link: "https://youtu.be/4k7IdSLxh6w",
+    link: 'https://youtu.be/4k7IdSLxh6w',
     image: darkSaasLandingPage,
   },
   {
-    company: "Innovative Co",
-    year: "2021",
-    title: "Light Saas Landing Page",
+    company: 'Innovative Co',
+    year: '2021',
+    title: 'Light Saas Landing Page',
     results: [
-      { title: "Boosted sales by 20%" },
-      { title: "Expanded customer reach by 35%" },
-      { title: "Increased brand awareness by 15%" },
+      { title: 'Boosted sales by 20%' },
+      { title: 'Expanded customer reach by 35%' },
+      { title: 'Increased brand awareness by 15%' },
     ],
-    link: "https://youtu.be/7hi5zwO75yc",
+    link: 'https://youtu.be/7hi5zwO75yc',
     image: lightSaasLandingPage,
   },
   {
-    company: "Quantum Dynamics",
-    year: "2023",
-    title: "AI Startup Landing Page",
+    company: 'Quantum Dynamics',
+    year: '2023',
+    title: 'AI Startup Landing Page',
     results: [
-      { title: "Enhanced user experience by 40%" },
-      { title: "Improved site speed by 50%" },
-      { title: "Increased mobile traffic by 35%" },
+      { title: 'Enhanced user experience by 40%' },
+      { title: 'Improved site speed by 50%' },
+      { title: 'Increased mobile traffic by 35%' },
     ],
-    link: "https://youtu.be/Z7I5uSRHMHg",
+    link: 'https://youtu.be/Z7I5uSRHMHg',
     image: aiStartupLandingPage,
   },
 ];
 
 export const ProjectsSection = () => {
-  return <div>Projects Section</div>;
+  return (
+    <section className='pb-16 lg:py-24'>
+      <div className='container'>
+        <div className='flex  justify-center'>
+          <p className='uppercase font-semibold tracking-widest bg-gradient-to-r from-emerald-300 to-sky-300 bg-clip-text text-transparent'>
+            Real-world results
+          </p>
+        </div>
+
+        <h1 className='text-center font-serif text-3xl mt-6 md:text-5xl'>
+          Featured Projects
+        </h1>
+        <p className='text-center text-white/50 mt-4 md:text-lg lg:text-xl max-w-md mx-auto'>
+          See how I transformed concepts into engaging digital experiences.
+        </p>
+        <div className='mt-10 flex flex-col gap-14 md:mt-20 md:gap-20'>
+          {portfolioProjects.map((project) => (
+            <div
+              key={project.year + project.title}
+              className='bg-gray-800 p-8  md:px-10 md:pt-12 lg:pt-16 lg:px-20 pb-0 rounded-3xl outline outline-3 outline-white/20 overflow-hidden relative -z-20 '
+            >
+              <div
+                className='inset-0 absolute -z-10 opacity-5'
+                style={{ backgroundImage: `url(${grainImage.src})` }}
+              ></div>
+              <div className='lg:grid lg:grid-cols-2 lg:gap-16'>
+                <div className='lg:pb-16'>
+                  <div className='flex gap-2 font-semibold uppercase text-sm tracking-wider bg-gradient-to-r from-emerald-300 to-sky-300 bg-clip-text text-transparent'>
+                    <span>{project.company}</span>
+                    <span>•</span>
+                    <span>{project.year}</span>
+                  </div>
+                  <h3 className='font-serif text-2xl mt-2 md:text-4xl md:pt-4'>
+                    {project.title}
+                  </h3>
+                  <hr className='border-white/5 mt-4 border-t-2 md:mt-5' />
+                  <ul className='mt-4 md:mt-5 flex flex-col gap-4'>
+                    {project.results.map((result) => (
+                      <li
+                        key={result.title}
+                        className='flex gap-2 md:text-base text-white/50'
+                      >
+                        <CheckCircleIcon className='size-5 md:size-6' />
+                        {result.title}
+                      </li>
+                    ))}
+                  </ul>
+                  <button className='h-12 bg-white text-gray-950 w-full md:w-auto px-6 rounded-2xl inline-flex items-center justify-center gap-2 mt-8'>
+                    <span className='font-semibold '>Visit Live Site</span>
+                    <span>
+                      <RightUpArrowIcon className='size-4' />
+                    </span>
+                  </button>
+                </div>
+                <div>
+                  <Image
+                    src={project.image}
+                    alt={project.title}
+                    className=' mt-8 -mb-4 md:-mb-0 lg:mt-0 lg:mb-0 lg:absolute lg:h-full lg:w-auto lg:max-w-none'
+                  />
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
 };
